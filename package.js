dce40583dfe4b5fd38f62563b676a02b09b5531a
JobTracking1 <jt1@jobtracking.com.au>
2015-12-17 20:04:08 +0000

    added summary

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'nekoanz:dropzone',
     version: '0.0.1',
     // Brief, one-line summary of the package.
-    summary: '',
+    summary: 'MeteorJS wrapper for DropzoneJS',
     // URL to the Git repository containing the source code for this package.
     git: '',
     // By default, Meteor will default to using README.md for documentation.
